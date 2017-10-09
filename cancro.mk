692d07f73fdc2991a9d0949a7ba752679fe1c03e
ShivamKumarJha <jha.shivam3@gmail.com>
2017-10-09 12:44:33 +0530

    cancro: Add HIDL manifest

--- cancro.mk
+++ cancro.mk
@@ -263,6 +263,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.software.print.xml:system/etc/permissions/android.software.print.xml
 
+# HIDL
+DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/manifest.xml
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
